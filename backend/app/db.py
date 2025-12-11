ba89c89eec994c4e2da55f0d4dcce1fbb278c38f
atharv9910gupta-coder <atharv9910gupta@gmail.com>
2025-12-11 20:22:50 +0530

    Update db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,1 +1,10 @@
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from .config import DATABASE_URL
+
+engine = create_engine(DATABASE_URL, echo=False, future=True)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+Base = declarative_base()
+metadata = MetaData()
 
